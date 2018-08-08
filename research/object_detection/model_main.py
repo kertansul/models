b99511c14b0b18c64395c4b93587db55f22eda26
Shawn Chen <shawn.chen@viscovery.com>
2018-08-08 02:16:21 +0000

    fix bug for object api while train on innovex

--- research/object_detection/model_main.py
+++ research/object_detection/model_main.py
@@ -18,6 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import matplotlib
+matplotlib.use('Agg')
+
 from absl import flags
 
 import tensorflow as tf
--- research/object_detection/utils/visualization_utils.py
+++ research/object_detection/utils/visualization_utils.py
@@ -623,7 +623,7 @@ def visualize_boxes_and_labels_on_image_array(
               class_name = category_index[classes[i]]['name']
             else:
               class_name = 'N/A'
-            display_str = str(class_name)
+            display_str = class_name.encode('utf8')
         if not skip_scores:
           if not display_str:
             display_str = '{}%'.format(int(100*scores[i]))
